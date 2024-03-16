b16e95f288040da2c7323bc8023548b12019d66f
spsalazar <156946827+spsalazar@users.noreply.github.com>
2024-03-16 00:04:20 +0000

    done?

--- main.hpp
+++ main.hpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int writeFile(string);
+int readFile(string);
 
 int writeFile(string fileName) {
 ofstream ofs;
@@ -28,4 +29,39 @@ ofstream ofs;
     }
 
     ofs.close();
+    return fileName;
+}
+
+int readFile(string fileName) {
+    ifstream ifs;
+
+    ifs.open("employee.txt");
+    if (!ifs)
+    {
+        cout << "File Open Error\n";
+        exit(0);
+    }
+
+    int empID;
+    int numOfEmp;
+    string empName;
+    string deptName;
+    double salary;
+    double total = 0;
+    double avg = 0;
+
+    ifs >> numOfEmp;
+    cout << numOfEmp << endl;
+
+    for (int i = 0; i < numOfEmp; i++) {
+        ifs >> empID >> empName >> deptName >> salary;
+        total += salary;
+        cout << "Employee ID: " << empID << " Name: " << empName << " Department: " << deptName << " Salary: " << salary << endl;
+    }
+
+    cout << "Total: " << total << endl;
+    avg = total / numOfEmp;
+    cout << "Average: " << avg << endl;
+
+    return fileName;
 }
