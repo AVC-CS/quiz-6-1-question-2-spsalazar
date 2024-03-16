8971bac9c79ae2294518495db6dba0c3779206ae
spsalazar <156946827+spsalazar@users.noreply.github.com>
2024-03-16 00:02:52 +0000

    test1

--- main.hpp
+++ main.hpp
@@ -1,3 +1,31 @@
-/***************************************************
- * Code your program here
- ***************************************************/
+#include <iostream>
+using namespace std;
+
+int writeFile(string);
+
+int writeFile(string fileName) {
+ofstream ofs;
+
+    ofs.open("employee.txt");
+    if (!ofs)
+    {
+        cout << "File Open Error\n";
+        exit(0);
+    }
+
+    int empID;
+    int numOfEmp;
+    string empName;
+    string deptName;
+    double salary;
+
+    cin >> numOfEmp;
+    ofs << numOfEmp << endl;
+
+    for (int i = 0; i < numOfEmp; i++) {
+        cin >> empID >> empName >> deptName >> salary;
+        ofs << empID << " " << empName << " " << deptName << " " << salary << endl;
+    }
+
+    ofs.close();
+}
